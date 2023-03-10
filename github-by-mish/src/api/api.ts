0b04186005689b706b441e715590b49e9a4e694d
MihlaliMgwebi <mihlalimgwebi@gmail.com>
2023-03-10 09:13:18 +0200

    feat: :alien: update api
    different endpoint, different response

--- github-by-mish/src/api/api.ts
+++ github-by-mish/src/api/api.ts
@@ -1,7 +1,7 @@
 
-export async function listPublicRepositories(): Promise<IRepositories | undefined> {
+export async function listPublicRepositories(): Promise<IPublicRepositories | undefined> {
     return fetch("https://api.github.com/repositories")
-        .then((response) => response.json() as Promise<IRepositories>)
+        .then((response) => response.json() as Promise<IPublicRepositories>)
         .then((data) => data)
         .catch((err) => {
             return undefined;
